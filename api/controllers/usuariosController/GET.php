4fff792dcda1cedc782d2627dcb5661e52dfd03e
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2024-10-17 19:56:42 -0300

    primeiro commit no branch de desenvolvimento

--- api/controllers/usuariosController/GET.php
+++ api/controllers/usuariosController/GET.php
@@ -2,68 +2,40 @@
 if ($acao == 'index' && $parametro == '') {
 
     // ---------------------------------------
-    // PEGA TODOS OS OPERADORES
+    // PEGA TODOS OS usuários
     // ---------------------------------------
 
-    
 
-    // if (!empty($_GET['codigos']) && $_GET['codigos'] === 'true') {
-    //     $db = DB::connect();
-    //     $sql = $db->prepare("SELECT usuarios.matricula, usuarios.nome from usuarios where usuarios.matricula = usuarios.matricula and usuarios.matricula < 6");
-    //     $sql->execute();
-    //     $obj = $sql->fetchAll(PDO::FETCH_ASSOC);
-    //     if (!$obj) {
-    //         $response = array(
-    //             "message" => "Nenhum usuário encontrado!"
-    //         );
-    //         echo json_encode($response);
-    //         exit;
-    //     }
+    if (!empty($_GET['turma']) && !empty($_GET['gerencia'])) {
 
+        $turma = $_GET['turma'];
+        $gerencia = $_GET['gerencia'];
 
-    //     echo json_encode($obj);
-    //     exit;
-    // }
+        $db = DB::connect();
 
+        // $sql = $db->prepare("SELECT * from operadores where operadores.matricula > 5");
+        // $sql->execute();
+        // $sql = $db->prepare("SELECT operadores.matricula, usuarios.nome, operadores.disponivel, usuarios.matriculasupervisor from operadores, usuarios");
+        $sql = $db->prepare("SELECT usuarios.matricula, usuarios.nome, usuarios.email, usuarios.gerencia, usuarios.turma, usuarios.url_perfil, usuarios.url_status from usuarios where usuarios.turma = ? and usuarios.gerencia = ?");
+        $sql->execute([$turma, $gerencia]);
+        $obj = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-    // if (!empty($_GET['turma']) && !empty($_GET['gerencia'])) {
+        if (!$obj) {
+            $response = array(
+                "error" => true,
+                "message" => "Nenhum usuário encontrado!"
+            );
+            echo json_encode($response);
+        } else {
+            $response = array(
+                "error" => false,
+                "message" => $obj
+            );
+            echo json_encode($response);
+        }
 
-    //     $turma = $_GET['turma'];
-    //     $gerencia = $_GET['gerencia'];
-
-    //     $db = DB::connect();
-
-    //     // $sql = $db->prepare("SELECT * from operadores where operadores.matricula > 5");
-    //     // $sql->execute();
-    //     // $sql = $db->prepare("SELECT operadores.matricula, usuarios.nome, operadores.disponivel, usuarios.matriculasupervisor from operadores, usuarios");
-    //     $sql = $db->prepare("SELECT operadores.matricula, usuarios.nome, usuarios.turma, gerencia.nome as gerencia, operadores.disponivel from usuarios, operadores, gerencia where operadores.matricula = usuarios.matricula and gerencia.id = usuarios.idgerencia and usuarios.matricula > 5 and usuarios.turma = ? and usuarios.idgerencia = ?");
-    //     $sql->execute([$turma, $gerencia]);
-    //     $obj = $sql->fetchAll(PDO::FETCH_ASSOC);
-
-    //     if (!$obj) {
-    //         $response = array(
-    //             "message" => "Nenhum operador encontrado!"
-    //         );
-    //         echo json_encode($response);
-    //         exit;
-    //     }
-
-    //     for ($i = 0; $i < count($obj); $i++) {
-
-
-    //         $sql = $db->prepare("SELECT * from operadores where operadores.matricula = ?");
-    //         $sql->execute([$obj[$i]['matricula']]);
-    //         $operador = $sql->fetch(PDO::FETCH_ASSOC);
-
-    //         unset($operador['matricula']);
-    //         unset($operador['disponivel']);
-
-    //         $obj[$i]['autorizadoOperar'] = $operador;
-    //     }
-
-    //     echo json_encode($obj);
-    //     exit;
-    // }
+        exit;
+    }
 
     if (empty($_GET['turma']) && empty($_GET['gerencia'])) {
         $db = DB::connect();
@@ -89,72 +61,56 @@ if ($acao == 'index' && $parametro == '') {
         exit;
     }
 
-    // if (empty($_GET['gerencia'])) {
+    if (empty($_GET['gerencia'])) {
 
-    //     $turma = $_GET['turma'];
+        $turma = $_GET['turma'];
 
-    //     $db = DB::connect();
-    //     $sql = $db->prepare("SELECT operadores.matricula, usuarios.nome, usuarios.turma, gerencia.nome as gerencia, usuarios.matriculasupervisor, operadores.disponivel from usuarios, operadores, gerencia where operadores.matricula = usuarios.matricula and gerencia.id = usuarios.idgerencia and usuarios.matricula > 5 and usuarios.turma = ?");
-    //     $sql->execute([$turma]);
-    //     $obj = $sql->fetchAll(PDO::FETCH_ASSOC);
+        $db = DB::connect();
+        $sql = $db->prepare("SELECT usuarios.matricula, usuarios.nome, usuarios.email, usuarios.gerencia, usuarios.turma, usuarios.url_perfil, usuarios.url_status from usuarios where usuarios.turma = ?");
+        $sql->execute([$turma]);
+        $obj = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 
-    //     if (!$obj) {
-    //         $response = array(
-    //             "message" => "Nenhum operador encontrado!"
-    //         );
-    //         echo json_encode($response);
-    //         exit;
-    //     }
+        if (!$obj) {
+            $response = array(
+                "error" => true,
+                "message" => "Nenhum usuário encontrado!"
+            );
+            echo json_encode($response);
+        } else{
+            $response = array(
+                "error" => false,
+                "message" => $obj
+            );
+            echo json_encode($obj);
+        }
+        exit;
+    }
 
-    //     for ($i = 0; $i < count($obj); $i++) {
+    if (empty($_GET['turma'])) {
 
-    //         $sql = $db->prepare("SELECT * from operadores where operadores.matricula = ?");
-    //         $sql->execute([$obj[$i]['matricula']]);
-    //         $operador = $sql->fetch(PDO::FETCH_ASSOC);
+        $gerencia = $_GET['gerencia'];
 
-    //         unset($operador['matricula']);
-    //         unset($operador['disponivel']);
+        $db = DB::connect();
+        $sql = $db->prepare("SELECT usuarios.matricula, usuarios.nome, usuarios.email, usuarios.gerencia, usuarios.turma, usuarios.url_perfil, usuarios.url_status from usuarios where usuarios.gerencia = ?");
+        $sql->execute([$gerencia]);
+        $obj = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-    //         $obj[$i]['autorizadoOperar'] = $operador;
-    //     }
-
-    //     echo json_encode($obj);
-    //     exit;
-    // }
-
-    // if (empty($_GET['turma'])) {
-
-    //     $gerencia = $_GET['gerencia'];
-
-    //     $db = DB::connect();
-    //     $sql = $db->prepare("SELECT operadores.matricula, usuarios.nome, usuarios.turma, gerencia.nome as gerencia, usuarios.matriculasupervisor, operadores.disponivel from usuarios, operadores, gerencia where operadores.matricula = usuarios.matricula and gerencia.id = usuarios.idgerencia and usuarios.matricula > 5 and usuarios.idgerencia = ?");
-    //     $sql->execute([$gerencia]);
-    //     $obj = $sql->fetchAll(PDO::FETCH_ASSOC);
-
-    //     if (!$obj) {
-    //         $response = array(
-    //             "message" => "Nenhum operador encontrado!"
-    //         );
-    //         echo json_encode($response);
-    //         exit;
-    //     }
-
-    //     for ($i = 0; $i < count($obj); $i++) {
-
-    //         $sql = $db->prepare("SELECT * from operadores where operadores.matricula = ?");
-    //         $sql->execute([$obj[$i]['matricula']]);
-    //         $operador = $sql->fetch(PDO::FETCH_ASSOC);
-
-    //         unset($operador['matricula']);
-    //         unset($operador['disponivel']);
-
-    //         $obj[$i]['autorizadoOperar'] = $operador;
-    //     }
-
-    //     echo json_encode($obj);
-    //     exit;
-    // }
+        if (!$obj) {
+            $response = array(
+                "error" => true,
+                "message" => "Nenhum usuário encontrado!"
+            );
+            echo json_encode($response);
+        } else {
+            $response = array(
+                "error" => false,
+                "message" => $obj
+            );
+            echo json_encode($obj);
+        }
+        exit;
+    }
 }
 
 if ($acao == 'show' && $parametro != '') {
