96b2ea8723fb9b5e1be3843631c1a51887c67bac
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2024-10-31 12:45:35 -0300

    utilizando o PDO e atualização do runtime para 7.2

--- api/database/DB.php
+++ api/database/DB.php
@@ -11,9 +11,7 @@ class DB
         $sslmode = getenv('DB_SSLMODE');
         $options = getenv('DB_OPTIONS');
 
-        // return new PDO("pgsql:host=$host;dbname=$base;user=$user;password=$pass;sslmode=$sslmode;options=$options");
-        $conn_string = "host=$host dbname=$base user=$user password=$pass sslmode=$sslmode options='--endpoint=$host'";
-        return pg_connect($conn_string);
+        return new PDO("pgsql:host=$host;dbname=$base;user=$user;password=$pass;sslmode=$sslmode;options=$options");
         // return new PDO("pgsql:host=$host;dbname=$base;user=$user;password=$pass");
         // return new PDO("pgsql:host=$host;dbname=$base", $user, $pass);
     }
